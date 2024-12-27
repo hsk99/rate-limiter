f6ff79366ab56ac66bc507515d747cdef752db2d
walkor <walkor@users.noreply.github.com>
2024-12-27 18:02:44 +0800

    Update RateLimitException.php

--- src/RateLimitException.php
+++ src/RateLimitException.php
@@ -5,6 +5,7 @@ namespace Webman\RateLimiter;
 use support\exception\BusinessException;
 use Webman\Http\Request;
 use Webman\Http\Response;
+use Throwable;
 
 class RateLimitException extends BusinessException
 {
@@ -19,5 +20,29 @@ class RateLimitException extends BusinessException
                 json_encode($json, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
         }
         return new Response($code, [], $message);
+    }
+
+    /**
+     * Translate message.
+     * @param string $message
+     * @param array $parameters
+     * @param string|null $domain
+     * @param string|null $locale
+     * @return string
+     */
+    protected function trans(string $message, array $parameters = [], ?string $domain = null, ?string $locale = null): string
+    {
+        $args = [];
+        foreach ($parameters as $key => $parameter) {
+            $args[":$key"] = $parameter;
+        }
+        try {
+            $message = trans($message, $args, $domain, $locale);
+        } catch (Throwable $e) {
+        }
+        foreach ($parameters as $key => $value) {
+            $message = str_replace(":$key", $value, $message);
+        }
+        return $message;
     }
 }
